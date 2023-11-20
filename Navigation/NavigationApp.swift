85041871def17ca328e38e42f619f38ee35e749a
shomil03 <shomil02020@gmail.com>
2023-11-20 22:11:20 +0530

    Initial Commit

---
+++ Navigation/NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  NavigationApp.swift
+//  Navigation
+//
+//  Created by Shomil Singh on 20/11/23.
+//
+
+import SwiftUI
+
+@main
+struct NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
